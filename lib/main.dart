0056c156b75f778a158bb6e3867bdd3539206571
khushbu <khushburajak17@gmail.com>
2025-01-17 18:23:58 +0545

    updated widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:scholarshuip_finder_app/view/auth/login_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:scholarshuip_finder_app/app/app.dart';
+import 'package:scholarshuip_finder_app/app/di/di.dart';
 
-void main() async{
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-     debugShowCheckedModeBanner: false,
-      title: 'Scholarship Finder',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: GoogleFonts.poppinsTextTheme(),
-      ),
-      home: LoginScreen(),
-    );
-  }
+  await initDependencies();
+  runApp(
+    App(),
+  );
 }
